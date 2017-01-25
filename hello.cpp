4dab8678053c8998832abb0037169d5717b0b93d
Marian Luparu <mluparu@microsoft.com>
2017-01-25 14:18:57 -0800

    sample prj

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <sqlite3.h>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello!" << std::endl;
+	//std::cout << sqlite3_libversion() << std::endl;
+
+	return 0;
+}
